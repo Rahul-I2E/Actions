116d1b3b25b590167690b6d59710d42e4d899c82
Rahul-I2E <rahul.bhawsar@i2econsulting.com>
2025-07-29 10:03:30 +0530

    Update GitHubActionApplicationTests.java

--- src/test/java/com/example/GitHubAction/GitHubActionApplicationTests.java
+++ src/test/java/com/example/GitHubAction/GitHubActionApplicationTests.java
@@ -2,6 +2,9 @@ package com.example.GitHubAction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 @SpringBootTest
 class GitHubActionApplicationTests {
